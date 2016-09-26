75882dd01634eeaf6107390e4cbffc522837f34d
DhruvRajesh <druvy@live.com>
2016-09-26 20:11:13 +0400

    A simple map app to help understanding the basics

---
+++ mapViewApp/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  mapViewApp
+//
+//  Created by Knowledge Is Power. on 8/23/16.
+//  Copyright © 2016 Dhruv Rajesh. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate
+{
+
+    @IBOutlet var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad()
+    {
+    
+        super.viewDidLoad()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+       
+        self.locationManager.startUpdatingLocation()
+        self.mapView.showsUserLocation = true
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // Location delegate methods
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+       self.locationManager.stopUpdatingLocation()
+    }
+    
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError)
+    {
+        print("Errors: " + error.localizedDescription)
+    }
+    
+    
+    
+    
+}
+
